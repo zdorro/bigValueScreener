f399a1f2b5e25ce2ace68646d01d7fa219e16f48
zdorro <90772559+zdorro@users.noreply.github.com>
2022-08-25 11:58:12 +0300

    В консоль выводятся объемы больше, чем 1

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,20 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  const baseUrl = "https://api.binance.com/api/v3/depth?symbol=BTCUSDT&limit=5000";
+  axios.get(baseUrl).then((res) => {
+    let firstArray = res.data.bids.map(function(name) {
+      return name[1]
+    })
+    firstArray.forEach(element => {
+      if (element > 50) {
+        console.log(element)
+      }
+    });
+  });
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>srccccc/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
