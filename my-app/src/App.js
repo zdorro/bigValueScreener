208dc9e7c306e98fd03f2e777a12ce6c80d85f37
AlekseyLu <luzyanin-ae@yandex.ru>
2022-08-25 22:03:36 +0300

    getValue Aleksey

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
+import 'regenerator-runtime/runtime';
+import axios from 'axios';
+
+const url = 'https://api.binance.com/api/v3/depth?symbol=BTCUSDT&limit=5000';
+/*
+  .then(res => console.log(res.data.asks))
+  .catch(error => console.log(error));
+*/
+const arr = [];
+class GetTotal {
+  async getUrl(url) {
+    try {
+      const res = await axios.get(url);
+
+      return res.data.asks.forEach(function(value) {
+        arr.push(value[1]);
+      });
+    } catch (error) {
+      console.log(error.massage);
+      return false;
+    }
+  }
+}
+
+const getTotal = new GetTotal();
+
+(async () => {
+  await getTotal.getUrl(url);
+
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i] > 30) {
+    console.log(arr[i]);
+    return arr[i];
+  }
+}
+})();
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>Assssssssssssrcccccccc/App.js</code> and save to reload.
+        <p class='text'>
+          async();
         </p>
         <a
           className="App-link"
