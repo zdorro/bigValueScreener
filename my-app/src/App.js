4bbf5ab3d486e1a80a2bf1ffd8d0aa2c426a9120
AlekseyLu <luzyanin-ae@yandex.ru>
2022-08-26 20:29:33 +0300

    color value

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,22 +1,47 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
+
+const url = 'https://api.binance.com/api/v3/depth?symbol=BTCUSDT&limit=300';
+
+
 function App() {
+
+  const [value, setValue] = useState([]);
+
+  useEffect(() => {
+      axios
+        .get(url)
+        .then((result) => {
+          setValue(result.data.asks);
+        });
+    }, []);
+
+     const newValue = value.map(elem => {     
+      return elem; 
+          });
+
+    const newValue2 = newValue.filter(val => {
+              if (val[1] > 1) {
+                return val;
+              }
+            })
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>srcccccccc/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        ДА ЕСТЬ ЖЕШЬ!
+          <div className="valueArr">
+            {newValue2.map(el => {
+              return (
+                //<div className='valueElem'>
+                    <div className={(el[1] > 2) ? 'valueTrue' : 'valueElem'}>Цена: {el[0]}; Наполненность: {el[1]}</div>
+                //</div>
+              )
+            })}
+          </div>
       </header>
     </div>
   );
