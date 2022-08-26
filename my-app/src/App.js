4ef49327608d31a09ffbf9c0e08684c135f65ea3
zdorro <90772559+zdorro@users.noreply.github.com>
2022-08-26 14:56:28 +0300

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,31 +1,34 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
 import logo from "./logo.svg";
 import "./App.css";
-import axios from "axios";
-import React, { useCallback, useEffect } from "react";
 
 function App() {
   const baseURL = "https://api.binance.com/api/v3/depth?symbol=BTCUSDT&limit=500";
-  const [qty, setPost] = React.useState(null);
+  const [signals, setSignals] = useState([]);
 
-  React.useEffect(() => {
+  useEffect(() => {
     axios.get(baseURL).then((res) => {
-      setPost(
-        res.data.bids.map(function (name) {
-          if (name[1] > 1) {
-            return <div className="qty_1">{name[1]}</div>
-          }
-        })
-      );
+
+      const { bids } = res.data;
+
+      const newSignals = bids
+      .map(([price, qty]) => ({ price, qty }))
+      .filter(({ qty }) => qty > 1);
+
+      setSignals(newSignals);
+
     });
   }, []);
-
-  if (!qty) return null;
 
   return (
     <div className="App">
       <header className="App-header">
         QTY:
-        <div className="qty">{qty}</div>
+        <div className="qty">
+          {signals.map(({price, qty})=> <div className="qty_1">{price}: {qty}</div>)}
+        </div>
       </header>
     </div>
   );
